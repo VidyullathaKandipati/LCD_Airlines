d73564f2816fb234d1b15d8317c296151403e784
VidyullathaKandipati <latha522@gmail.com>
2017-02-07 15:08:26 +1100

    Basic model, collection, and router working :)

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,5 +2,10 @@ var app = app || {};
 
 $(document).ready(function(){
 
+  app.appRouter = new app.AppRouter();
+  Backbone.history.start();
+
+  app.flights = new app.Flights();
+  app.flights.fetch();
 
 });
